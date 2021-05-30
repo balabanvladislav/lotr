bd53d2616daa56e4cd880ada64e2946bd5e96884
vladislav <balabanvislav@gmail.com>
2021-05-30 20:36:59 +0300

    first commit

---
+++ LOTR/Migrations/Configuration.cs
@@ -0,0 +1,30 @@
+namespace LOTR.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using LOTR.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LOTR.Domain.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(LOTR.Domain.ApplicationDbContext context)
+        {
+            AddUsers(context);
+        }
+        void AddUsers(LOTR.Domain.ApplicationDbContext context)
+        {
+            var user = new ApplicationUser { UserName = "admin@gmail.com" };
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+            um.Create(user, "password");
+        }
+    }
+}
